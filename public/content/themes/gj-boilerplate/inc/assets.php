01a55109998fd3277647a8ea5bbcf4fc916cd536
pshampine <pshampine@gunnjerkens.com>
2013-11-25 11:03:25 -0800

    Bugfix: derped hard on directory

--- public/content/themes/gj-boilerplate/inc/assets.php
+++ public/content/themes/gj-boilerplate/inc/assets.php
@@ -8,7 +8,7 @@ function enqueue_scripts() {
 	wp_enqueue_script('main', get_bloginfo('template_directory') . '/js/main.js', array('jquery'), null, true);
 
 	// HTML5 & Responsive Fallbacks
-	wp_enqueue_script('modernizr', get_bloginfo('template_directory') . '/js/libs/modernizr.js', false, null, false);
-	wp_enqueue_script('respond', get_bloginfo('template_directory') . '/js/libs/respond.min.js', false, null, false);
+	wp_enqueue_script('modernizr', get_bloginfo('template_directory') . '/js/modernizr.js', false, null, false);
+	wp_enqueue_script('respond', get_bloginfo('template_directory') . '/js/respond.min.js', false, null, false);
 }
 add_action('get_header', 'enqueue_scripts');
