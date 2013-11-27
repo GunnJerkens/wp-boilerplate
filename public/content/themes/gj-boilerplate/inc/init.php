5da1e1c4399d19e2e33a23ee8071abc9c3eb720e
pshampine <pshampine@gunnjerkens.com>
2013-11-27 12:08:40 -0800

    removed all feed links

--- public/content/themes/gj-boilerplate/inc/init.php
+++ public/content/themes/gj-boilerplate/inc/init.php
@@ -49,6 +49,9 @@ remove_action( 'wp_head', 'rsd_link');
 remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 remove_action( 'wp_head', 'wp_generator');
+remove_action( 'wp_head', 'feed_links' );
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
 
 // Adds post thumbnails to theme
 add_theme_support( 'post-thumbnails' );
