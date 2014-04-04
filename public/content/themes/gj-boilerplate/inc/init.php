a0e9e30aa8ef0fe703c076e47dec6027a4fb2ca8
Patrick Shampine <pshampine@gunnjerkens.com>
2014-04-04 15:56:54 -0700

    Limiting analytics to only populate in wp_head in production env

--- public/content/themes/gj-boilerplate/inc/init.php
+++ public/content/themes/gj-boilerplate/inc/init.php
@@ -17,29 +17,23 @@ if( WP_PASSWORD_PROTECT == true ){
 update_option('upload_url_path', '/shared');
 
 // Loads Google Analytics
-$google_analytics_id = 'UA-XXXXXXXX-X'; // override this value in functions.php
-function google_analytics() {
-  global $env_default, $google_analytics_id;
-  $default_hostname = preg_replace('/^https?:\/\//', '', $env_default['hostname']);
-  ?>
-  <!-- Google Analytics -->
-  <script>
-  if(['<?php echo $default_hostname ?>','www.<?php echo $default_hostname ?>']
-    .indexOf(window.location.hostname) > -1
-    && window.location.search.search('&preview=true') == -1
-  ){
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+if($environment['name'] === 'production') {
+  $google_analytics_id = 'UA-XXXXXXXX-X'; // override this value in functions.php
+  function google_analytics() {
+    global $env_default, $google_analytics_id;
+    $default_hostname = preg_replace('/^https?:\/\//', '', $env_default['hostname']); ?>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', '<?php echo $google_analytics_id ?>', '<?php echo $default_hostname ?>');
-    ga('send', 'pageview');
+      ga('create', '<?php echo $google_analytics_id ?>', '<?php echo $default_hostname ?>');
+      ga('send', 'pageview');
+    </script><?php
   }
-  </script>
-<?php
-} // google_analytics
-add_action('wp_head','google_analytics');
+  add_action('wp_head','google_analytics');
+}
 
 // Removes manifest/rsd/shortlink from wp_head
 remove_action( 'wp_head', 'wlwmanifest_link');
