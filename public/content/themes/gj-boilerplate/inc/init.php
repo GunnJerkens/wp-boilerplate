3176aa929919e6c954728d789587fb6bfdd828b1
Patrick Shampine <pshampine@gunnjerkens.com>
2015-01-05 09:22:33 -0800

    Updated node_modules to latest

--- public/content/themes/gj-boilerplate/inc/init.php
+++ public/content/themes/gj-boilerplate/inc/init.php
@@ -86,7 +86,7 @@ if(constant('WP_ENV') == 'local') {
   function add_browser_sync() {
     echo '
       <script type=\'text/javascript\' id="__bs_script__">//<![CDATA[
-        document.write("<script async src=\'//HOST:3000/browser-sync/browser-sync-client.1.7.1.js\'><\/script>".replace(/HOST/g, location.hostname).replace(/PORT/g, location.port));
+          document.write("<script async src=\'//HOST:3000/browser-sync/browser-sync-client.1.8.3.js\'><\/script>".replace(/HOST/g, location.hostname).replace(/PORT/g, location.port));
       //]]></script>
     ';
   }
