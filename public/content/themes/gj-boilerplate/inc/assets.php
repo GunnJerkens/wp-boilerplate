2509f0ecaa09a5ea7473a9587b506554fb7b746c
Patrick Shampine <pshampine@gunnjerkens.com>
2015-01-19 13:58:15 -0800

    Updated cdn version to bs 3.3.2

--- public/content/themes/gj-boilerplate/inc/assets.php
+++ public/content/themes/gj-boilerplate/inc/assets.php
@@ -12,7 +12,7 @@ function enqueue_scripts() {
   wp_deregister_script('jquery');
   wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', false, null, true);
   // If you opt to use bootstrap over the cdn, make sure to delete the ENTIRE bootstrap folder from /gj-boilerplate/js/src/
-  // wp_enqueue_script('bootstrap', 'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js', array('jquery'), null, true);
+  // wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js', array('jquery'), null, true);
 
   $main = '/js/main.js';
   $mainFilePath = get_template_directory() . $main;
