270925672325ef815254562b2996da4db2bf93cc
Patrick Shampine <pshampine@gunnjerkens.com>
2015-06-11 15:43:16 -0700

    Mocked out methods for gjOptions

--- public/content/themes/gj-boilerplate/inc/options.php
+++ public/content/themes/gj-boilerplate/inc/options.php
@@ -2,18 +2,82 @@
 
 class gjOptions {
 
-  function __construct() {
-    add_action('admin_menu', array(&$this,'gj_options_admin_actions'));
+  /**
+   * Private class vars
+   *
+   * @var $prefix string
+   */
+  private $prefix;
+
+  /**
+   * Constructor
+   */
+  function __construct()
+  {
+    $this->prefix = "gj_options_";
   }
 
-  function gj_options_admin_actions() {
-    add_options_page( 'GJ Options', 'GJ Options', 'administrator', 'gj_options', array(&$this,'gj_options_admin_options'));
+  /**
+   * Loads admin
+   *
+   * @return void
+   */
+  public function loadAdmin()
+  {
+    add_action('admin_menu', array(&$this,'adminPage'));
   }
 
-  function gj_options_admin_options() {
+  /**
+   * Adds the menu option
+   *
+   * @return void
+   */
+  private function adminPage() {
+    add_options_page( 'GJ Options', 'GJ Options', 'administrator', 'gj_options', array(&$this, 'adminTemplates'));
+  }
+
+  /**
+   * Includes the admin template
+   *
+   * @return void
+   */
+  private function adminTemplates() {
     include(get_template_directory() . '/options/gj-options.php');
+  }
+
+  /**
+   * Saves options to the database
+   *
+   * @param array
+   *
+   * @return object
+   */
+  public function updateOptions()
+  {
+
+  }
+
+  /**
+   * Retrieves options to the database
+   *
+   * @return object
+   */
+  public function requestOptions()
+  {
+
+  }
+
+  /**
+   * Deletes options from the database
+   *
+   * @return void
+   */
+  public function deleteOptions()
+  {
+
   }
 
 }
 
-new gjOptions();
+(new gjOptions())->loadAdmin();
+
