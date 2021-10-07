46f1f04f27f1dea20e4358483c1321cc7cff0abc
Sam Richardson <srichardson@GJIWK118-Sam-Richardson.local>
2021-10-07 15:11:58 -0700

    new tweaks

---
+++ public/content/themes/gj-boilerplate/partials/svg-icons.php
@@ -0,0 +1,19 @@
+<!-- Menu Icon -->
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="menu-defs" class="hidden">
+  <defs>
+    <symbol id="menu" viewBox="0 0 24 24">
+      <title>menu</title>
+      <path d="M3,6h18v1.6H3V6z M3,12.8v-1.6h18v1.6H3z M3,18v-1.6h18V18H3z"/>
+    </symbol>
+  </defs>
+</svg>
+<!-- Close Icon -->
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="close-defs" class="hidden">
+  <defs>
+    <symbol id="close" viewBox="0 0 35 35">
+      <title>close</title>
+      <rect x="-6.82" y="17.07" width="48.63" height="0.86" transform="translate(17.5 -7.25) rotate(45)"/>
+      <rect x="-6.82" y="17.07" width="48.63" height="0.86" transform="translate(42.25 17.5) rotate(135)"/>
+    </symbol>
+  </defs>
+</svg>
